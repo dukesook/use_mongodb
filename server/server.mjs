9ef5f399029d502649d8e3f5aa141e663d77bc1c
dukesook <devonsookhoo14@gmailcom>
2025-03-25 21:04:23 -0600

    send form data

--- server/server.mjs
+++ server/server.mjs
@@ -13,6 +13,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), '../public/index.html'));
 });
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+  console.log('Form submitted');
+  // log request:
+  console.log(req.body);
+  res.send('Form received');
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
